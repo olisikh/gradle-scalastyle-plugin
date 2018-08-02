3332be0b08cd8e620ee63888f25a4462ea2cd35b
Oleksii Lisikh <lizard5472@gmail.com>
2018-08-03 00:50:38 +0200

    Code style improvements

--- gradle-scalastyle-plugin_source/src/main/groovy/org/github/ngbinh/scalastyle/ScalaStylePlugin.groovy
+++ gradle-scalastyle-plugin_source/src/main/groovy/org/github/ngbinh/scalastyle/ScalaStylePlugin.groovy
@@ -35,10 +35,15 @@ import org.scalastyle.XmlOutput
 @Slf4j
 class ScalaStylePlugin implements Plugin<Project> {
 
+    private Project project
+    private ScalaStyle extension
+
     private ScalaStyleUtils scalaStyleUtils = new ScalaStyleUtils()
 
     void apply(Project project) {
-        def extension = new ScalaStyle(project.container(ScalaStyleSourceSet))
+        this.project = project
+        this.extension = new ScalaStyle(project.container(ScalaStyleSourceSet))
+
         project.extensions.add('scalaStyle', extension)
         project.configurations.create('scalaStyle')
                 .setVisible(false)
@@ -46,93 +51,98 @@ class ScalaStylePlugin implements Plugin<Project> {
                 .setDescription('Scalastyle libraries to be used for this project.')
 
         project.afterEvaluate {
-            ScalastyleConfiguration globalConfig = extension.config ?
-                    loadScalaStyleConfig(extension.config) :
-                    null
+            setupScalaStyle()
+        }
+    }
 
-            def srcSets = project.sourceSets.findAll { !it.scala.srcDirs.isEmpty() }
-                    .collectEntries { [it.name, it.scala.srcDirs] }
+    private def setupScalaStyle() {
+        ScalastyleConfiguration scalaStyleConfig = extension.config ?
+                loadScalaStyleConfig(extension.config) :
+                null
 
-            def scalaStyleTasks = srcSets.findResults {
-                def sourceSetName = it.key
-                def srcDirs = it.value
+        def srcSets = project.sourceSets.findAll { !it.scala.srcDirs.isEmpty() }
+                .collectEntries { [it.name, it.scala.srcDirs] }
 
-                def overrides = extension.sourceSets.find { it.name == sourceSetName } ?:
-                        new ScalaStyleSourceSet(sourceSetName) // create dummy object to not fail with NPE
+        def scalaStyleTasks = srcSets.findResults {
+            def srcSetName = it.key as String
+            def srcDirs = it.value as List<File>
 
-                def skip = overrides.skip != null ? overrides.skip : extension.skip
-                if (!skip) {
-                    project.task(type: SourceTask, "scalaStyle${sourceSetName.capitalize()}") {
-                        group = 'verification'
-                        description = "Runs scalastyle checks on source set ${sourceSetName}."
+            createScalaStyleTask(srcSetName, srcDirs, scalaStyleConfig)
+        }
 
-                        source = srcDirs
+        project.task('scalaStyle') {
+            group = 'verification'
+            description = "Runs scalastyle checks."
 
-                        doLast {
-                            try {
-                                def startMs = System.currentTimeMillis()
+            dependsOn scalaStyleTasks
+        }
+    }
 
-                                ScalastyleConfiguration usedConfig = overrides.config ?
-                                        loadScalaStyleConfig(overrides.config) :
-                                        globalConfig
+    private def createScalaStyleTask(String srcSetName, List<File> srcDirs, ScalastyleConfiguration scalaStyleConfig) {
+        def overrides = extension.sourceSets.find { it.name == srcSetName } ?:
+                new ScalaStyleSourceSet(srcSetName) // create dummy object to not fail with NPE
 
-                                def encoding = overrides.inputEncoding ?: extension.inputEncoding
-                                def filesToProcess = scalaStyleUtils.getFilesToProcess(
-                                        srcDirs.collect { project.file(it) }, encoding)
-                                def messages = scalaStyleUtils.checkFiles(usedConfig, filesToProcess)
+        def skip = overrides.skip != null ? overrides.skip : extension.skip
+        if (!skip) {
+        project.task(type: SourceTask, "scalaStyle${srcSetName.capitalize()}") {
+            group = 'verification'
+            description = "Runs scalastyle checks on source set ${srcSetName}."
 
-                                def config = scalaStyleUtils.readConfig()
-                                def verbose = overrides.verbose != null ? overrides.verbose : extension.verbose
-                                def quiet = overrides.quiet != null ? overrides.quiet : extension.quiet
+            source = srcDirs
 
-                                def outputResult = new TextOutput(config, verbose, quiet).output(messages)
+            doLast {
+                try {
+                    def startMs = System.currentTimeMillis()
 
-                                def outputFile = overrides.output ?
-                                        project.file(overrides.output) :
-                                        project.file("${project.buildDir.absolutePath}/scalastyle/${sourceSetName}/scalastyle-check.xml")
+                    def encoding = overrides.inputEncoding ?: extension.inputEncoding
+                    def filesToProcess = scalaStyleUtils.getFilesToProcess(srcDirs, encoding)
+                    def messages = scalaStyleUtils.checkFiles(overrides.config ?
+                            loadScalaStyleConfig(overrides.config) :
+                            scalaStyleConfig, filesToProcess)
 
-                                log.debug("Saving to outputFile={}", outputFile.canonicalPath)
-                                def outputEncoding = overrides.outputEncoding ?: extension.outputEncoding
+                    def config = scalaStyleUtils.readConfig()
+                    def verbose = overrides.verbose != null ? overrides.verbose : extension.verbose
+                    def quiet = overrides.quiet != null ? overrides.quiet : extension.quiet
 
-                                XmlOutput.save(config, outputFile.absolutePath, outputEncoding, messages)
+                    def outputResult = new TextOutput(config, verbose, quiet).output(messages)
 
-                                def stopMs = System.currentTimeMillis()
-                                if (!quiet) {
-                                    log.info("Processed {} file(s)", outputResult.files())
-                                    log.warn("Found {} warnings", outputResult.warnings())
-                                    log.error("Found {} errors", outputResult.errors())
-                                    log.info("Finished in {} ms", stopMs - startMs)
-                                }
+                    def outputFile = overrides.output ?
+                            project.file(overrides.output) :
+                            project.file("${project.buildDir.absolutePath}/scalastyle/${srcSetName}/scalastyle-check.xml")
 
-                                def failOnWarning = overrides.failOnWarning != null ? overrides.failOnWarning : extension.failOnWarning
+                    log.debug("Saving to outputFile={}", outputFile.canonicalPath)
+                    def outputEncoding = overrides.outputEncoding ?: extension.outputEncoding
 
-                                def violations = outputResult.errors() + ((failOnWarning) ? outputResult.warnings() : 0)
-                                def failOnViolation = overrides.failOnViolation != null ? overrides.failOnViolation : extension.failOnViolation
-                                if (violations > 0) {
-                                    if (failOnViolation) {
-                                        throw new GradleException("You have " + violations + " Scalastyle violation(s).")
-                                    } else {
-                                        log.warn("Scalastyle:check violations detected but failOnViolation set to " + failOnViolation)
-                                    }
-                                } else {
-                                    log.debug("Scalastyle:check no violations found")
-                                }
-                            } catch (Exception ex) {
-                                throw new GradleException("Scalastyle check error", ex)
-                            }
-                        }
+                    XmlOutput.save(config, outputFile.absolutePath, outputEncoding, messages)
+
+                    def stopMs = System.currentTimeMillis()
+                    if (!quiet) {
+                        log.info("Processed {} file(s)", outputResult.files())
+                        log.warn("Found {} warnings", outputResult.warnings())
+                        log.error("Found {} errors", outputResult.errors())
+                        log.info("Finished in {} ms", stopMs - startMs)
                     }
-                } else {
-                    null
+
+                    def failOnWarning = overrides.failOnWarning != null ? overrides.failOnWarning : extension.failOnWarning
+
+                    def violations = outputResult.errors() + ((failOnWarning) ? outputResult.warnings() : 0)
+                    def failOnViolation = overrides.failOnViolation != null ? overrides.failOnViolation : extension.failOnViolation
+                    if (violations > 0) {
+                        if (failOnViolation) {
+                            throw new GradleException("You have " + violations + " Scalastyle violation(s).")
+                        } else {
+                            log.warn("Scalastyle:check violations detected but failOnViolation set to " + failOnViolation)
+                        }
+                    } else {
+                        log.debug("Scalastyle:check no violations found")
+                    }
+                } catch (Exception ex) {
+                    throw new GradleException("Scalastyle check error", ex)
                 }
             }
-
-            project.task('scalaStyle') {
-                group = 'verification'
-                description = "Runs scalastyle checks."
-
-                dependsOn scalaStyleTasks
-            }
+        }
+        } else {
+            null
         }
     }
 
